7a1206ca4a4b495f6587d42e9393dd1b42dac004
VLC <valentinleonchristensen@gmail.com>
2019-05-13 14:42:19 +0200

    moved mouse clicker method to gameObhect

--- src/java/model/Army.java
+++ src/java/model/Army.java
@@ -1,26 +1,19 @@
 package model;
 
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Army extends GameObject {
-    private ObservableList<Node> children;
-    private Rectangle rectangle;
-
 
     public Army() {
-        rectangle = new Rectangle(0,0,100,100);
+        Rectangle rectangle = new Rectangle(0,0,100,100);
         rectangle.setFill(Color.CYAN);
-        children = this.getChildren();
         children.add(rectangle);
-
-        this.setOnMouseClicked(event -> System.out.println(this + " was clicked"));
-
     }
 
 
-
-
+    @Override
+    protected void handleMouseClick() {
+        System.out.println(this + " was clicked.");
+    }
 }
--- src/java/model/GameObject.java
+++ src/java/model/GameObject.java
@@ -1,12 +1,21 @@
 package model;
 
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 
 // GameObject er for at samle alle ting som skal tegnes på skærmen, så de er nemmere at behandle ensartet.
 public abstract class GameObject extends StackPane {
+    protected ObservableList<Node> children = this.getChildren();
+
+    public GameObject(){
+        this.setOnMouseClicked(event -> handleMouseClick());
+    }
 
 
-    public GameObject() {
+
+    protected void handleMouseClick() {
 
     }
+
 }
